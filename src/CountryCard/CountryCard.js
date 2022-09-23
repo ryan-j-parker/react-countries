15aae0d2337d64081cc5ee0271ea57d03c94b339
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-22 19:40:34 -0700

    redeploy

--- src/CountryCard/CountryCard.js
+++ src/CountryCard/CountryCard.js
@@ -5,8 +5,6 @@ export default function CountryCard({ name, iso2 }) {
   return (
     <div className="country-container">
       <div className="country-card">
-        {/* <img className="flag"
-          src={`https://flagcdn.com/48x36/${iso2.toLowerCase()}.png`} /> */}
         <img className="flag"
           src={`https://flagcdn.com/108x81/${iso2.toLowerCase()}.png`}
           width="108"
